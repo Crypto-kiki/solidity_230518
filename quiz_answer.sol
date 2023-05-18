7789eccc669bc9cd0ee370236766378ccc1ad1a4
kiki <ejo2123@naver.com>
2023-05-18 15:49:13 +0900

    abi.encodePacked

--- quiz_answer.sol
+++ quiz_answer.sol
@@ -32,7 +32,7 @@ contract Q5 {
         return a;
     }
 
-    /* for에 조건문 삭제가능.
+    /* for에 조건문 삭제가능
     function getLength(uint _n) public pure returns(uint) {
         for(uint i=0; ; i++) {
             if(_n < 10 ** i){
@@ -41,6 +41,26 @@ contract Q5 {
         }
     }
     */
+
+    //abcd -> 0x 61 62 63 64 임. 따라서 길이를 먼저 구해야됨.
+    function bytesToBytesArray2(bytes memory _bytes) public pure returns(bytes1[] memory) {
+        bytes1[] memory _bytes1 = new bytes1[](_bytes.length);
+        for(uint i = 0; i < _bytes1.length; i++) {
+            _bytes1[i] = _bytes[i];
+        }
+        return _bytes1;
+    }
+
+    function stringToBytes1Array(string memory _string) public pure returns(uint, string[] memory) {
+        bytes1[] memory b_array = new bytes1[](bytes(_string).length);
+        b_array = bytesToBytesArray2(bytes(_string));
+        
+        string[] memory s_array = new string[](b_array.length);
+        for(uint i = 0; i < s_array.length; i++) {
+            s_array[i] = string(abi.encodePacked(b_array[i]));
+        }
+        return (s_array.length, s_array);
+    }
 
 }
 
@@ -82,7 +102,96 @@ contract WHILE {
 }
 
 contract REVERSEARRAY {
-
     
+    function reverse1(uint[] calldata numbers) public pure returns(uint[] memory) {
+        uint[] memory _reverse = new uint[](numbers.length);
+        for(uint i = 0; i < numbers.length; i++) {
+            _reverse[i] = numbers[numbers.length - i - 1];
+        }
+        return _reverse;
+    }
+    /*
+    calldata는 원본을 갖고와서 쓰는거고 memory는 카피해서 사용. 더 힘든게 memory임.
+    copy해서 사용하기 때문.
+    memory는 값이 변할 수 있음. 카피기 때문. calldata는 안변함. 원본이기 때문.
+    하지만 위 코드에서 보면 numbers memory는 값이 안변함. 값을 주기는 해도!
+    따라서 calldata로 써도 됨. calldata가 가스비 덜 듬.
+    */
+
+    // 아래 코드는 앞뒤 1:1로 reverse 시키는 코드임.
+    function reverse2(uint[] memory numbers) public pure returns(uint[] memory) {
+        for(uint i = 0; i < numbers.length / 2; i++) {
+            (numbers[i], numbers[numbers.length - 1 - i]) = (numbers[numbers.length - 1 - i], numbers[i]);
+        }
+        return numbers;
+    }
+
+}
+
+
+contract BYTES_LENGTH {
+// bytes는 special한 array 타입임. = 길이를 구할 수 있다. bytes <-> string 가능.
+    function getLength1(bytes1 a) public pure returns(uint) {
+        return a.length;
+    }  // 0x11 -> 1
+    function getLength2(bytes2 a) public pure returns(uint) {
+        return a.length;
+    }  // 0x1111 -> 2
+    function getLength3(bytes3 a) public pure returns(uint) {
+        return a.length;
+    }  // 0x111111 -> 3
+
+    function getLength(bytes memory a) public pure returns(uint) {
+        return a.length;
+    }  // 동적으로 들어간 만큼 길이가 나옴.
+
+    /*
+    abc에서 첫 글자를 뽑아내려면?
+    abc를 bytes로 바꾸고 bytes1을 꺼내서 다시 string으로.
+    그런데 쉽게 하는법이 있었음.
+    */
+
+    function bytes1ToString(bytes1 a) public pure returns(string memory) {
+        string memory _a = string(abi.encodePacked(a));
+        return _a;
+    }
+
+    // 정적 -> 동적
+    function bytesToBytesArray1(bytes memory _a) public pure returns(bytes1, bytes1) {
+        return (_a[0], _a[1]);
+    }
+
+
+    //abcd -> 0x 61 62 63 64 임. 따라서 길이를 먼저 구해야됨.
+    function bytesToBytesArray2(bytes memory _bytes) public pure returns(bytes1[] memory) {
+        bytes1[] memory _bytes1 = new bytes1[](_bytes.length);
+        for(uint i = 0; i < _bytes1.length; i++) {
+            _bytes1[i] = _bytes[i];
+        }
+        return _bytes1;
+    }
+
+    function stringToBytes1Array(string memory _string) public pure returns(uint, string[] memory) {
+        bytes1[] memory b_array = new bytes1[](bytes(_string).length);
+        b_array = bytesToBytesArray2(bytes(_string));
+        
+        string[] memory s_array = new string[](b_array.length);
+        for(uint i = 0; i < s_array.length; i++) {
+            s_array[i] = string(abi.encodePacked(b_array[i]));
+        }
+        return (s_array.length, s_array);
+    }
+
+    // 아래 코드는 abcde를 a, b, c, d, e로 나눠주고 나눠진 a, b, c, d, e를 합쳐서 abcde로 다시 나오게도 가능함.
+    function stringToBytes1Array2(string memory _string) public pure returns(uint, string[] memory, string memory) {
+        bytes1[] memory b_array = new bytes1[](bytes(_string).length);
+        b_array = bytesToBytesArray2(bytes(_string));
+        
+        string[] memory s_array = new string[](b_array.length);
+        for(uint i = 0; i < s_array.length; i++) {
+            s_array[i] = string(abi.encodePacked(b_array[i]));
+        }
+        return (s_array.length, s_array, string(abi.encodePacked(b_array)));
+    }
 
 }
